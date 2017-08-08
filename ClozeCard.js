cdd179bd04903099fdaf71d5882a7e2206acbb49
kmboyle <kevin.michael.boyle@gmail.com>
2017-08-08 12:38:23 -0400

    back end API

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,6 +1,11 @@
 function ClozeCard(text, cloze) {
-    this.text = text;
-    this.cloze = cloze;
+    //implementing scope safe constructor
+    if (this instanceof ClozeCard) {
+        this.text = text;
+        this.cloze = cloze;
+    } else {
+        return new ClozeCard(text, cloze);
+    }
 };
 
 module.exports = ClozeCard;
--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
 var questions = require("./flashCards.json");
 var inquirer = require('inquirer');
+var jsonfile = require('jsonfile');
+var file = 'flashCards.json';
 //variable inc is used to keep count of number of cards
+//if running default demo's, inc will be used as the array index, otherwise, count will be used as the array index
 var inc = 0;
 //prototype to accept a full text property
 ClozeCard.prototype.fullText = this.fullText;
@@ -16,25 +19,27 @@ function prompt() {
             type: "list",
             name: "typeCard",
             message: "What type of card would you like to make:",
-            choices: ["Basic Card", "Cloze Card"]
+            choices: ["Basic Card", "Cloze Card", "Basic Card Demo", "Cloze Card Demo"]
         },
         {
             type: "input",
             name: "number",
-            message: "How many cards would you like to make?"
+            message: "How many cards would you like?"
 
         }
     ]).then(function(answers) {
-        console.log(answers.typeCard);
         if (answers.typeCard === "Basic Card") {
             basicCard(answers.number);
-        } else {
-            console.log("CLOZE!!");
+        } else if (answers.typeCard === "Cloze Card") {
             clozeCard(answers.number);
-        };
+        } else if (answers.typeCard === "Basic Card Demo") {
+            basicDefault(answers.number);
+        } else {
+            clozeDefault(answers.number);
+        }
     });
 }
-
+//will build the basic card front and back and give user option to run cards through basicAnswer function
 function basicCard(count) {
 
     if (count > 0) {
@@ -67,14 +72,43 @@ function basicCard(count) {
         }]).then(function(answers) {
             if (answers.run) {
                 //here is where the user will get their questions displayed back to them
-                console.log("Ok");
+                console.log("Ok.  Here are your " + inc + " card(s):");
+                count = 0;
+                console.log(questions);
+                console.log(count);
+                basicAnswer(inc, count);
             } else {
                 console.log("No? Well, Alrighty then.");
             }
         });
     }
 }
+//this function provides a testing scenario for each "flashcard" the user created
+function basicAnswer(inc, count) {
+    if (inc > 0) {
+        inc--;
+        inquirer.prompt([{
+            type: "input",
+            message: questions.basic[count].front,
+            name: "guess"
+        }]).then(function(answers) {
 
+            console.log(count);
+            if (answers.guess === questions.basic[count].back) {
+                console.log("Correct!");
+            } else {
+                console.log("Sorry, Incorrect.  Correct answer is: " + questions.basic[count].back);
+            }
+            count++;
+            basicAnswer(inc, count);
+        });
+    } else {
+        repeat();
+    }
+
+}
+
+//This option allows user to build the cloze card flashcards and gives an option to run through the clozeAnswer function
 function clozeCard(count) {
     if (count > 0) {
         count--;
@@ -107,22 +141,8 @@ function clozeCard(count) {
             if (answers.run) {
                 //here is where the user will get their questions displayed back to them
                 console.log("Ok.  Here are your " + inc + " card(s):");
-                count = inc;
-                console.log(questions);
-                console.log(count);
-                if (count > 0) {
-                    count--;
-                    inquirer.prompt([{
-                        type: "input",
-                        message: questions.cloze[count].partialText,
-                        name: "guess"
-                    }]).then(function(answers) {
-
-                        if (answers.guess === questions.cloze[count].cloze) {
-                            console.log("Correct!");
-                        }
-                    });
-                }
+                count = 0;
+                clozeAnswer(inc, count);
             } else {
                 console.log("No? Well, Alrighty then.");
             }
@@ -131,20 +151,113 @@ function clozeCard(count) {
 }
 //function to remove the cloze in the full text and save new string to partial text property
 function clozeDeleted(guesser, removal, cards) {
-
-    var stringArray = guesser.split(" ");
-    if (stringArray.includes(removal)) {
-        console.log("Here it goes");
-        console.log(stringArray);
-        cards.partialText = guesser.replace(removal, "...");
-        console.log(cards.partialText);
-        questions.cloze.push(cards.partialText);
-
+    var stringArray = guesser.toLowerCase().split(" ");
+    if (stringArray.includes(removal.toLowerCase())) {
+        cards.partialText = guesser.toLowerCase().replace(removal.toLowerCase(), "...");
     } else {
         return console.log("Oops, that is not in the sentence.");
     }
-
-
 }
+//allows the user a way to run cloze flashcards
+function clozeAnswer(inc, count) {
+    if (inc > 0) {
+        inc--;
+        inquirer.prompt([{
+            type: "input",
+            message: questions.cloze[count].partialText,
+            name: "guess"
+        }]).then(function(answers) {
+
+            if (answers.guess.toLowerCase() === questions.cloze[count].cloze.toLowerCase()) {
+                console.log("Correct!");
+            } else {
+                console.log("Sorry, Incorrect.  Correct answer is: " + questions.cloze[count].cloze);
+            }
+            count++;
+            clozeAnswer(inc, count);
+        });
+    } else {
+        repeat();
+    }
+}
+//function that runs through cloze card examples from the flashCards.json file
+function clozeDefault(count) {
+
+
+    if (count > 0) {
+
+        count--;
+        jsonfile.readFile(file, "utf8", function(err, obj) {
+            if (err) {
+                console.log(err);
+            } else {
+                inc = Math.floor(Math.random() * obj.clozeDefault.length);
+                clozeDeleted(obj.clozeDefault[inc].text, obj.clozeDefault[inc].cloze, obj.clozeDefault[inc]);
+
+                inquirer.prompt([{
+                    type: "input",
+                    message: obj.clozeDefault[inc].partialText,
+                    name: "guess"
+                }]).then(function(answers) {
+
+                    if (answers.guess.toLowerCase() === obj.clozeDefault[inc].cloze.toLowerCase()) {
+                        console.log("Correct!");
+                    } else {
+                        console.log("Sorry, Incorrect.  Correct answer is: " + obj.clozeDefault[inc].cloze);
+                    }
+
+                    clozeDefault(count);
+                });
+            }
+        });
+    } else {
+        repeat();
+    }
+}
+//function that runs through basic card examples from the flashCards.json file
+function basicDefault(count) {
+
+    if (count > 0) {
+        count--;
+        jsonfile.readFile(file, "utf8", function(err, obj) {
+            if (err) {
+                console.log(err);
+            } else {
+                inc = Math.floor(Math.random() * obj.basicDefault.length);
+                console.log(inc);
+                inquirer.prompt([{
+                    type: "input",
+                    message: obj.basicDefault[inc].front,
+                    name: "guess"
+                }]).then(function(answers) {
+                    if (answers.guess.toLowerCase() === obj.basicDefault[inc].back.toLowerCase()) {
+                        console.log("Correct!");
+                    } else {
+                        console.log("Sorry, Incorrect.  Correct answer is: " + obj.basicDefault[inc].back);
+                    }
+                    basicDefault(count);
+                });
+            }
+        });
+    } else {
+        repeat();
+    }
+}
+//function to continue running the application
+function repeat() {
+    inquirer.prompt([{
+        type: "confirm",
+        message: "Would you like to go back to the main menu?",
+        name: "run"
+    }]).then(function(answers) {
+        if (answers.run) {
+            prompt();
+        } else {
+            console.log("OK, thank you, and have a nice day!");
+        }
+    });
+}
+
+
 //ask user to select type of card they want to make
 prompt();
